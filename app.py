146c05f1e0e46a877c6cb4568bc21cbc725fa7fa
Md Tariqul Islam <programmertariq@gmail.com>
2022-07-10 11:48:36 +0600

    creating and removing sheets

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import openpyxl
 
 wb = openpyxl.load_workbook("transactions.xlsx")
+# for printing sheet names
+# print(wb.sheetnames)
+
+sheet = wb['Sheet1']
+
+# creating and removing worksheets with index
+wb.create_sheet('Sheet2', 0)
+wb.remove(wb["Sheet2"])
+
 print(wb.sheetnames)
+
+
 
