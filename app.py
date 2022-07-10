42d53af86392679236318f503da39bf2fadd4b09
Md Tariqul Islam <programmertariq@gmail.com>
2022-07-10 11:53:11 +0600

    changing the value of a cell

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ wb = openpyxl.load_workbook("transactions.xlsx")
 sheet = wb['Sheet1']
 
 # creating and removing worksheets with index
-wb.create_sheet('Sheet2', 0)
-wb.remove(wb["Sheet2"])
-
+# wb.create_sheet('Sheet2', 0)
+# wb.remove(wb["Sheet2"])
 print(wb.sheetnames)
 
+# Accessing an individual cell or a range of cells
+# passing coordinate of a cell
+cell = sheet['A1'] 
+# changing the value of a cell
+cell.value = "id"
+print(cell.value)
 
 
